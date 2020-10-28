f406c7f5bf2cc4d548c60b04445d5683c4305445
Geert van de Langenberg <geert.van.de.langenberg@freshheads.com>
2020-10-28 10:32:56 +0100

    Upgrade next 10

--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -1,9 +1,33 @@
+import { Asset, ContentfulCollection } from 'contentful';
 import { FC } from 'react';
+import { contentfulClient } from '../../src/api/contentfulClient';
+import { wrapper } from '../../src/redux/store';
+import Image from 'next/image'
 
-const Article: FC = () => {
+type Props = {
+    items: Asset[]
+}
+
+const Article: FC<Props> = ({items}) => {
     return (
-        <h1>Article</h1>
+        <>
+            <h1>Article</h1>
+            {JSON.stringify(items)}
+        </>
     )
 };
+
+export const getServerSideProps = wrapper.getServerSideProps(
+    async () => {
+        const client = contentfulClient();
+        const entries = await client.getEntries();
+
+        return {
+            props: {
+                items: entries.items
+            }
+        };
+    }
+);
 
 export default Article;
